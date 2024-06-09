1fde877d2dd4c29b451905978804384d56d0ecf5
Sebastien LALLEMAND <sld@redhat.com>
2024-06-09 22:07:02 +0200

    file name param

--- scaffolder-templates/quarkus-rest-postgresql-template/skeleton/src/main/java/${{values.java_package_name}}/${{values.java_package_name}}Resource.java
+++ scaffolder-templates/quarkus-rest-postgresql-template/skeleton/src/main/java/${{values.java_package_name}}/${{values.java_package_name}}Resource.java
@@ -5,12 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("${{values.api_endpoint}}")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello from RESTEasy Reactive";
     }
 }
