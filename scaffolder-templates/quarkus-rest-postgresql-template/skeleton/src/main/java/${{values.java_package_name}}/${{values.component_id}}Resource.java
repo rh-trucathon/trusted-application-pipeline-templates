f1775acaf8d4416d66f11bcfd364931feed3e514
Sebastien LALLEMAND <sld@redhat.com>
2024-06-10 14:46:16 +0200

    up

--- scaffolder-templates/quarkus-rest-postgresql-template/skeleton/src/main/java/${{values.java_package_name}}/${{values.component_id}}Resource.java
+++ scaffolder-templates/quarkus-rest-postgresql-template/skeleton/src/main/java/${{values.java_package_name}}/${{values.component_id}}Resource.java
@@ -1,9 +1,9 @@
 package ${{values.java_package_name}};
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("${{values.api_endpoint}}")
 public class ${{values.component_id}}Resource {
